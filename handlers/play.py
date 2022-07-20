a603fa032b0fc9c5d6da1e6962fe30cc5f044ebd
Mahinabi <108423502+Mahinabi@users.noreply.github.com>
2022-07-20 23:52:30 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -55,7 +55,7 @@ async def play(_, message: Message):
 
     await message.delete()
 
-    fallen = await message.reply("» ᴘʀᴏᴄᴇssɪɴɢ​... ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ʙᴀʙʏ🔎")
+    fallen = await message.reply("» ᴘʀᴏᴄᴇssɪɴɢ​... ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ʙᴀʙᴜ💞 👀")
 
     chumtiya = message.from_user.mention
 
