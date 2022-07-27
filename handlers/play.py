d9f0839a3249b6e05cd8f95dd42c4830154c1035
Mahinabi <108423502+Mahinabi@users.noreply.github.com>
2022-07-28 00:20:31 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -157,7 +157,7 @@ async def play(_, message: Message):
             return await fallen.edit(
                 "» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ🤦🏻‍♂️"
             )
-        await fallen.edit("🔎")
+        await fallen.edit("CAACAgUAAx0CYhdEbwACTFZi4YhZdBmfiS-Vq-kmhGQRzZESAAONAwACPctQVUPe1lBjtLIaHgQ")
         query = message.text.split(None, 1)[1]
         # print(query)
         try:
